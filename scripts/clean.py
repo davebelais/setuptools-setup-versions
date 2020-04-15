88212ffc7055c6eea10076a87caa234c116876e4
David Belais <david.belais@nike.com>
2020-04-15 12:29:10 -0700

    Update to accommodate ~= operator

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,8 +1,6 @@
-# !python3.7
+# !/usr/bin/env python3
 
 """
-@author: David Belais <david@belais.me>
-
 This script cleans up some temporary files created by `setuptools`, `tox`, and
 `pytest`.
 """
@@ -15,26 +13,20 @@ from urllib.parse import urljoin
 
 os.chdir(urljoin(__file__, '../'))
 
-package = __file__.split('/')[-3]
+module_name = __file__.split('/')[-3].replace('-', '_')
 
 for file_or_directory in (
-    'dist', 'build', '%s.egg-info' % package,
-    '.tox', '.cache',
-    '.pytest_cache'
+    'dist', 'build', '%s.egg-info' % module_name,
+    '.cache', '.pytest_cache', '.mypy_cache', '.tox'
 ):
     if os.path.exists(file_or_directory):
-
         command = (
             'git rm -r -f --cached --ignore-unmatch "%s"' %
             file_or_directory
         )
-
         print(command)
-
         status, output = getstatusoutput(command)
-
         if status != 0:
             raise OSError(output)
-
         if os.path.exists(file_or_directory):
             shutil.rmtree(file_or_directory)
