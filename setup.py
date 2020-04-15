a2764bd9363bfc4154c0c121f00c86896e22aa52
David Belais <david.belais@nike.com>
2020-04-15 12:28:42 -0700

    Update to accommodate ~= operator

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='setuptools-setup-versions',
-    version="0.0.32",
+    version="0.0.33",
     description=(
         "Automatically update setup.py `install_requires` version numbers"
         "for PIP packages"
@@ -13,18 +13,18 @@ setup(
     keywords='setuptools install_requires version',
     packages=find_packages(),
     install_requires=[
-        "setuptools>=46.1.3",
-        "pip>=20.0.2",
-        "more-itertools>=8.2.0"
+        "setuptools~=46.1.3",
+        "pip~=20.0.2",
+        "more-itertools~=8.2.0"
     ],
     extras_require={
         "test": [
-            "pytest>=5.4.1"
+            "pytest~=5.4.1"
         ],
         "dev": [
-            "pytest>=5.4.1",
-            "wheel>=0.34.2",
-            "readme-md-docstrings>=0.0.10"
+            "pytest~=5.4.1",
+            "wheel~=0.34.2",
+            "readme-md-docstrings~=0.0.10"
         ]
     }
 )
